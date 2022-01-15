dba9027d17918eb5cf7d5f250d3d266e14f7b256
sAm <m.a.mofidian@hotmail.co.uk>
2022-01-15 15:45:02 +0330

    add run in handler

--- handler.py
+++ handler.py
@@ -1,4 +1,5 @@
 import sys
+from functions import huffman_tree, encode, decode, code_generator, is_efficient
 
 
 def start():
@@ -16,3 +17,32 @@ def start():
         # call recursive to receive valid input(y/n)
         print('Invalid Input')
         return start()
+
+
+def run():
+    """
+    factory
+    :return:
+    """
+    # call start
+    start()
+
+    # receive text
+    text = input("please input your text:\n")
+
+    # create huffman tree
+    tree = huffman_tree(text)
+
+    # create code dictionary from huffman tree
+    codes = code_generator(tree)
+
+    # encode
+    encoded_text = encode(text, codes)
+    print('encoding result:', encoded_text)
+
+    # decode
+    decoded = decode(encoded_text, tree)
+    print('decoding result:', decoded)
+
+    # call function recursively
+    return run()
