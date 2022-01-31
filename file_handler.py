0729674fc187da2b94d799c8b43f22dbd5da8ced
sAm <m.a.mofidian@hotmail.co.uk>
2022-01-31 17:52:26 +0330

    add encode method (use in another method)

--- file_handler.py
+++ file_handler.py
@@ -2,4 +2,22 @@ from functions import *
 
 
 class FileHandler:
-    pass
+
+    @staticmethod
+    def __encode(text):
+        """
+        encoding file content -- use in compress func
+        :param text:
+        :return:
+        """
+        # make dictionary of chars and their repetition-count
+        chars_dict = chars_count_dict(text)
+
+        # create huffman tree
+        tree = huffman_tree(chars_dict)
+
+        # create code dictionary from huffman tree
+        codes = code_generator(tree)
+
+        # return encoded text
+        return encode(text, codes)
